ee1171448427be7652a9e4290c0a2e9266a77075
hussain-ahmed-siddiqui <hussain.siddiqui.5th@gmail.com>
2025-03-23 07:33:17 +0500

    First Commit.

---
+++ data_manipulation/src/main/java/com/hussain/data_manipulation/DataForgerApplication.java
@@ -0,0 +1,13 @@
+package com.hussain.data_manipulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataForgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataForgerApplication.class, args);
+	}
+
+}
---
+++ data_manipulation/src/test/java/com/hussain/data_manipulation/DataForgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussain.data_manipulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataForgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
